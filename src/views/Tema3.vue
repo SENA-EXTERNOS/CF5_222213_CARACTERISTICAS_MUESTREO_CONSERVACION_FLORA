3ae9ba8c963816b55b112833a9132543add566fa
elrilacho <camilobr17@gmail.com>
2023-04-03 15:34:17 -0500

    Componentel isto

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -9,7 +9,7 @@
     
     figure(data-aos="zoom-in")
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/9FK8m7u2jCY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     h4(data-aos="fade-right") Muestreos técnicos
     
